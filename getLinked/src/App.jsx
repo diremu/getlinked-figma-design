f66b1b511264f70814646eebc33b8b5c17bd1173
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-06-02 19:42:38 +0100

    Completed the footer component

--- getLinked/src/App.jsx
+++ getLinked/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/navbar/navbar'
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <>
     <div className=' bg-[#150e28] w-full min-h-[100vh] text-white'>
       <Navbar />
+      <Footer />
     </div>
     </>
   )
