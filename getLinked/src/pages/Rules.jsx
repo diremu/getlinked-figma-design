e35c9d62ba2f39cf97b2d01ab18cfbb407940575
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-06-05 08:27:24 +0100

    Completed the rules and guidlines page

--- getLinked/src/pages/Rules.jsx
+++ getLinked/src/pages/Rules.jsx
@@ -1,7 +1,18 @@
+import rules from '../images/rules.png'
+
 const Rules = () => {
     return (
-        <>
-        </>
+        <div className='px-6 md:px-10 flex flex-col md:flex-row-reverse md:py-8'>
+            <div className='py-4'>
+                <img src={rules} alt="rules" className='w-[40vw]' />
+            </div>
+            <div className='w-[40vw]'>
+                <h3 className='text-center  md:text-left font-bold text-xl md:text-2xl'>Rules and <br className='block' /><span className='text-[#d434fe]'>Guidelines</span></h3>
+                <p className="text-sm leading-relaxed pt-4 pb-8">
+                    Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!
+                </p>
+            </div>
+        </div>
     )
 }
 
--- getLinked/src/router.jsx
+++ getLinked/src/router.jsx
@@ -1,5 +1,6 @@
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
+import Rules from "./pages/Rules";
 import { Sponsors } from "./pages/Sponsors";
 
 
@@ -8,7 +9,8 @@ export default function Router() {
         <div className=" bg-[#150e28] w-full min-h-[100vh] text-white">
             {/* <Navbar /> */}
             {/* <Sponsors /> */}
-            <Footer />
+            {/* <Footer /> */}
+            <Rules />
         </div>
     )
 }
